9e0e4bd469cd9aacff43620db3dcf1456ab57ee5
Facundo-Banegaz <Banegaz@users.noreply.github.com>
2023-10-07 18:52:08 -0300

    Subiendo cambios a github, se crearon las capas de_ dominio, negocio y presentacion

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        //atributos
+        int  Id {set; get; }
+        string  Codigo { set; get; }
+
+        string Nombre { set; get; }
+        string Descripcion { set; get; }
+        Marca Marca { set; get; }
+        Categoria Categoria { set; get; }
+        string Imagen {set; get; }
+        decimal Precio { set; get; }
+
+    }
+}
