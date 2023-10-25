ac16fc988eece7728b36ba41761909b3c6921a72
Facundo-Banegaz <Banegaz@users.noreply.github.com>
2023-10-25 01:03:46 -0300

    subiendo

--- GestorDeCatalogos/FrmCargar.Designer.cs
+++ GestorDeCatalogos/FrmCargar.Designer.cs
@@ -32,15 +32,11 @@
             this.gbx_campos = new System.Windows.Forms.GroupBox();
             this.cbo_marca = new System.Windows.Forms.ComboBox();
             this.cbo_categoria = new System.Windows.Forms.ComboBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.txt_nombre = new System.Windows.Forms.TextBox();
             this.txt_descripcion = new System.Windows.Forms.TextBox();
-            this.txt_img = new System.Windows.Forms.TextBox();
-            this.txt_precio = new System.Windows.Forms.TextBox();
             this.label14 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
             this.txt_codigo = new System.Windows.Forms.TextBox();
@@ -49,6 +45,10 @@
             this.btn_guardar = new FontAwesome.Sharp.IconButton();
             this.btn_img_local = new FontAwesome.Sharp.IconButton();
             this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txt_img = new System.Windows.Forms.TextBox();
+            this.txt_precio = new System.Windows.Forms.TextBox();
             this.gbx_campos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_img)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
@@ -57,24 +57,24 @@
             // gbx_campos
             // 
             this.gbx_campos.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.gbx_campos.Controls.Add(this.cbo_marca);
-            this.gbx_campos.Controls.Add(this.cbo_categoria);
             this.gbx_campos.Controls.Add(this.label9);
             this.gbx_campos.Controls.Add(this.label10);
+            this.gbx_campos.Controls.Add(this.txt_img);
+            this.gbx_campos.Controls.Add(this.txt_precio);
+            this.gbx_campos.Controls.Add(this.cbo_marca);
+            this.gbx_campos.Controls.Add(this.cbo_categoria);
             this.gbx_campos.Controls.Add(this.label11);
             this.gbx_campos.Controls.Add(this.label12);
             this.gbx_campos.Controls.Add(this.label13);
             this.gbx_campos.Controls.Add(this.txt_nombre);
             this.gbx_campos.Controls.Add(this.txt_descripcion);
-            this.gbx_campos.Controls.Add(this.txt_img);
-            this.gbx_campos.Controls.Add(this.txt_precio);
             this.gbx_campos.Controls.Add(this.label14);
             this.gbx_campos.Controls.Add(this.label15);
             this.gbx_campos.Controls.Add(this.txt_codigo);
             this.gbx_campos.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gbx_campos.Location = new System.Drawing.Point(12, 12);
             this.gbx_campos.Name = "gbx_campos";
-            this.gbx_campos.Size = new System.Drawing.Size(527, 1007);
+            this.gbx_campos.Size = new System.Drawing.Size(527, 1041);
             this.gbx_campos.TabIndex = 2;
             this.gbx_campos.TabStop = false;
             this.gbx_campos.Text = "Detalle de Articulos:";
@@ -87,7 +87,7 @@
             this.cbo_marca.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbo_marca.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbo_marca.FormattingEnabled = true;
-            this.cbo_marca.Location = new System.Drawing.Point(19, 830);
+            this.cbo_marca.Location = new System.Drawing.Point(19, 875);
             this.cbo_marca.Name = "cbo_marca";
             this.cbo_marca.Size = new System.Drawing.Size(477, 37);
             this.cbo_marca.TabIndex = 6;
@@ -100,36 +100,10 @@
             this.cbo_categoria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbo_categoria.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbo_categoria.FormattingEnabled = true;
-            this.cbo_categoria.Location = new System.Drawing.Point(19, 935);
+            this.cbo_categoria.Location = new System.Drawing.Point(19, 980);
             this.cbo_categoria.Name = "cbo_categoria";
             this.cbo_categoria.Size = new System.Drawing.Size(477, 37);
             this.cbo_categoria.TabIndex = 7;
-            // 
-            // label9
-            // 
-            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label9.AutoSize = true;
-            this.label9.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.label9.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(16, 578);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(92, 22);
-            this.label9.TabIndex = 76;
-            this.label9.Text = "Url Imagen:";
-            // 
-            // label10
-            // 
-            this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label10.AutoSize = true;
-            this.label10.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.label10.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(15, 690);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(61, 22);
-            this.label10.TabIndex = 75;
-            this.label10.Text = "Precio:";
             // 
             // label11
             // 
@@ -138,7 +112,7 @@
             this.label11.AutoSize = true;
             this.label11.BackColor = System.Drawing.SystemColors.HighlightText;
             this.label11.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(24, 792);
+            this.label11.Location = new System.Drawing.Point(24, 837);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(60, 22);
             this.label11.TabIndex = 74;
@@ -151,7 +125,7 @@
             this.label12.AutoSize = true;
             this.label12.BackColor = System.Drawing.SystemColors.HighlightText;
             this.label12.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(15, 284);
+            this.label12.Location = new System.Drawing.Point(15, 576);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(102, 22);
             this.label12.TabIndex = 73;
@@ -185,32 +159,11 @@
             this.txt_descripcion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txt_descripcion.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_descripcion.Location = new System.Drawing.Point(19, 337);
+            this.txt_descripcion.Location = new System.Drawing.Point(19, 629);
             this.txt_descripcion.Multiline = true;
             this.txt_descripcion.Name = "txt_descripcion";
-            this.txt_descripcion.Size = new System.Drawing.Size(477, 220);
+            this.txt_descripcion.Size = new System.Drawing.Size(477, 180);
             this.txt_descripcion.TabIndex = 3;
-            // 
-            // txt_img
-            // 
-            this.txt_img.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_img.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_img.Location = new System.Drawing.Point(19, 626);
-            this.txt_img.Name = "txt_img";
-            this.txt_img.Size = new System.Drawing.Size(477, 32);
-            this.txt_img.TabIndex = 4;
-            // 
-            // txt_precio
-            // 
-            this.txt_precio.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_precio.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_precio.Location = new System.Drawing.Point(19, 736);
-            this.txt_precio.Name = "txt_precio";
-            this.txt_precio.Size = new System.Drawing.Size(477, 32);
-            this.txt_precio.TabIndex = 5;
-            this.txt_precio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_precio_KeyPress_1);
             // 
             // label14
             // 
@@ -219,7 +172,7 @@
             this.label14.AutoSize = true;
             this.label14.BackColor = System.Drawing.SystemColors.HighlightText;
             this.label14.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label14.Location = new System.Drawing.Point(15, 896);
+            this.label14.Location = new System.Drawing.Point(15, 941);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(85, 22);
             this.label14.TabIndex = 66;
@@ -255,7 +208,7 @@
             this.pbx_img.BackColor = System.Drawing.SystemColors.HighlightText;
             this.pbx_img.Location = new System.Drawing.Point(788, 159);
             this.pbx_img.Name = "pbx_img";
-            this.pbx_img.Size = new System.Drawing.Size(581, 524);
+            this.pbx_img.Size = new System.Drawing.Size(581, 558);
             this.pbx_img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbx_img.TabIndex = 4;
             this.pbx_img.TabStop = false;
@@ -272,7 +225,7 @@
             this.btn_cancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btn_cancelar.IconSize = 45;
             this.btn_cancelar.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btn_cancelar.Location = new System.Drawing.Point(1097, 931);
+            this.btn_cancelar.Location = new System.Drawing.Point(1097, 948);
             this.btn_cancelar.Name = "btn_cancelar";
             this.btn_cancelar.Size = new System.Drawing.Size(440, 70);
             this.btn_cancelar.TabIndex = 78;
@@ -294,7 +247,7 @@
             this.btn_guardar.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btn_guardar.IconSize = 45;
             this.btn_guardar.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btn_guardar.Location = new System.Drawing.Point(594, 931);
+            this.btn_guardar.Location = new System.Drawing.Point(594, 948);
             this.btn_guardar.Name = "btn_guardar";
             this.btn_guardar.Size = new System.Drawing.Size(440, 70);
             this.btn_guardar.TabIndex = 77;
@@ -316,7 +269,7 @@
             this.btn_img_local.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btn_img_local.IconSize = 40;
             this.btn_img_local.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btn_img_local.Location = new System.Drawing.Point(788, 705);
+            this.btn_img_local.Location = new System.Drawing.Point(788, 722);
             this.btn_img_local.Name = "btn_img_local";
             this.btn_img_local.Size = new System.Drawing.Size(581, 70);
             this.btn_img_local.TabIndex = 79;
@@ -329,12 +282,58 @@
             // 
             this.errorProvider.ContainerControl = this;
             // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label9.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(16, 296);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(92, 22);
+            this.label9.TabIndex = 80;
+            this.label9.Text = "Url Imagen:";
+            // 
+            // label10
+            // 
+            this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label10.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(15, 432);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 22);
+            this.label10.TabIndex = 79;
+            this.label10.Text = "Precio:";
+            // 
+            // txt_img
+            // 
+            this.txt_img.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_img.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_img.Location = new System.Drawing.Point(19, 339);
+            this.txt_img.Name = "txt_img";
+            this.txt_img.Size = new System.Drawing.Size(477, 32);
+            this.txt_img.TabIndex = 77;
+            // 
+            // txt_precio
+            // 
+            this.txt_precio.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_precio.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_precio.Location = new System.Drawing.Point(19, 478);
+            this.txt_precio.Name = "txt_precio";
+            this.txt_precio.Size = new System.Drawing.Size(477, 32);
+            this.txt_precio.TabIndex = 78;
+            // 
             // FrmCargar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.ClientSize = new System.Drawing.Size(1561, 1031);
+            this.ClientSize = new System.Drawing.Size(1561, 1065);
             this.Controls.Add(this.btn_img_local);
             this.Controls.Add(this.btn_cancelar);
             this.Controls.Add(this.btn_guardar);
@@ -357,15 +356,11 @@
         private System.Windows.Forms.GroupBox gbx_campos;
         private System.Windows.Forms.ComboBox cbo_marca;
         private System.Windows.Forms.ComboBox cbo_categoria;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.TextBox txt_nombre;
         private System.Windows.Forms.TextBox txt_descripcion;
-        private System.Windows.Forms.TextBox txt_img;
-        private System.Windows.Forms.TextBox txt_precio;
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.TextBox txt_codigo;
@@ -374,5 +369,9 @@
         private FontAwesome.Sharp.IconButton btn_guardar;
         private FontAwesome.Sharp.IconButton btn_img_local;
         private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txt_img;
+        private System.Windows.Forms.TextBox txt_precio;
     }
 }
